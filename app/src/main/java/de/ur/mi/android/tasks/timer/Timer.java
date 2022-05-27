18365e83fea1801410850735c5dad4ae54b30ac6
RandomIntStudios <48419836+RandomIntStudios@users.noreply.github.com>
2022-05-27 17:57:51 +0200

    Adjust project for starter-code

--- app/src/main/java/de/ur/mi/android/tasks/timer/Timer.java
+++ app/src/main/java/de/ur/mi/android/tasks/timer/Timer.java
@@ -17,50 +17,12 @@ import de.ur.mi.android.tasks.timer.broadcast.TimerBroadcastListener;
 public class Timer implements Runnable {
 
     public static String TIME_KEY = "TIME_KEY";
-    private boolean isRunning;
-    private int time;
-    private static final int TICK_RATE = 1000;
-    private ScheduledFuture<?> scheduledFuture;
-    private final TimerBroadcastListener listener;
 
-    public Timer (TimerBroadcastListener listener)
+    public Timer ()
     {
-        this.listener = listener;
-        isRunning = false;
+        //TODO: Passen sie den Konstruktor so an, dass er sich für die Verwendung eignet.
     }
 
-    /**
-     * Über diese Methode kann festgelegt werden, wie lange der Timer läuft
-     * @param time Laufzeit des Timers, in Sekunden.
-     */
-    public void setTime(int time){
-        this.time = time;
-    }
-
-    /**
-     * Über diese Methode wird der Timer gestartet, es wird ein Sheduled Executor erzeugt, welcher
-     * anschließdend die entprechende Run-Methode des übergebenen Runnables in reglemäßigen
-     * Abständen (hier: jede Sekunde) ausführt. Der ScheduledExecutor liefert uns ein Objekt des Typs
-     * SheduledFuture, welches jederzeit beendet werden kann, um die weitere Ausführung des
-     * Executors abzubrechen.
-     */
-    public void start()
-    {
-        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-        scheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(this, TICK_RATE, TICK_RATE, TimeUnit.MILLISECONDS);
-        isRunning = true;
-    }
-
-    /**
-     * Hält den Timer an und cancelled das ScheduledFuture.
-     */
-    public void stop(){
-        if (isRunning){
-            scheduledFuture.cancel(true);
-            isRunning = false;
-            listener.onTimerCancelled();
-        }
-    }
 
     /**
      * Die run-Methode setzt bei jeder Ausführung die Zeit um 1 herab, wenn sie 0 erreicht wird die
@@ -68,14 +30,7 @@ public class Timer implements Runnable {
      */
     @Override
     public void run() {
-        time--;
-        if (time > 0){
-            listener.onTimerUpdate(time);
-        }
-        else {
-            listener.onTimerFinished();
-            scheduledFuture.cancel(true);
-        }
+        //TODO Implementieren Sie hier die Funktionalität des Timers
     }
 
     /**
--- app/src/main/java/de/ur/mi/android/tasks/timer/TimerActivity.java
+++ app/src/main/java/de/ur/mi/android/tasks/timer/TimerActivity.java
@@ -14,10 +14,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import de.mi.timer.R;
 import de.ur.mi.android.tasks.timer.broadcast.TimerBroadcastListener;
 import de.ur.mi.android.tasks.timer.broadcast.TimerBroadcastReceiver;
-import de.ur.mi.android.tasks.timer.notifications.NotificationHelper;
 import de.ur.mi.android.tasks.timer.prefs.TimerState;
 import de.ur.mi.android.tasks.timer.prefs.TimerStateStorage;
-import de.ur.mi.android.tasks.timer.service.TimerService;
 
 /**
  * Die MainActivity der App, über die der Timer gestartet und gestoppt wird, sowie die
@@ -39,7 +37,6 @@ public class TimerActivity extends AppCompatActivity implements TimerBroadcastLi
         stateStorage = new TimerStateStorage(getApplicationContext());
         initUI();
         initBroadcasts();
-        setupNotifications();
     }
 
     //region Initialization
@@ -65,13 +62,6 @@ public class TimerActivity extends AppCompatActivity implements TimerBroadcastLi
         broadcastReceiver = new TimerBroadcastReceiver(this);
     }
 
-    /**
-     * Erstellt einen Notification-Channel, damit die App Notifications anzeigen kann.
-     */
-    private void setupNotifications() {
-        NotificationHelper notificationHelper = new NotificationHelper(this);
-        notificationHelper.createNotificationChannel();
-    }
 
     //endregion
 
@@ -79,21 +69,6 @@ public class TimerActivity extends AppCompatActivity implements TimerBroadcastLi
     protected void onStart() {
         super.onStart();
         registerBroadcastReceiver();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        updateButtonStates();
-    }
-
-    /**
-     * passt die Buttons so an, dass sie den richtigen Status haben, je nachdem ob gerade ein Timer läuft oder nicht.
-     */
-    private void updateButtonStates() {
-        TimerState currentState = stateStorage.getTimerState();
-        btnStartTimer.setEnabled(!currentState.value);
-        btnStopTimer.setEnabled(currentState.value);
     }
 
 
@@ -127,7 +102,7 @@ public class TimerActivity extends AppCompatActivity implements TimerBroadcastLi
      * Stoppt den Timer, indem der Service beendet wird.
      */
     private void stopTimer() {
-        stopService(new Intent(this, TimerService.class));
+
     }
 
     /**
@@ -135,28 +110,10 @@ public class TimerActivity extends AppCompatActivity implements TimerBroadcastLi
      * Andernfalls wird ein Toast angezeigt mit der Information die Eingaben zu berichtigen.
      */
     private void startTimer() {
-        if (checkInputs()) {
-            int[] setTime = getSetTime();
-            int timeInSeconds = (setTime[0] * 60 * 60) + (setTime[1] * 60) + (setTime[2]);
-            if (timeInSeconds == 0) {
-                Toast.makeText(this, getString(R.string.toast_time_not_set), Toast.LENGTH_SHORT).show();
-            } else {
-                startTimerForTime(timeInSeconds);
-            }
-            resetEdits();
-        } else {
-            Toast.makeText(this, R.string.toast_incorrect_input, Toast.LENGTH_SHORT).show();
-        }
+        //TODO: Wenn der Start-Button geklickt wird soll überprüft werden ob alle Inputs richtig sind
+        //TODO: Ist dies der Fall, kann die Eingabe ausgelesen werden und an startTimerForTime gereicht werden
     }
 
-    /**
-     * Setzt die Eingabefelder zurück.
-     */
-    private void resetEdits() {
-        edtHours.setText("");
-        edtMinutes.setText("");
-        edtSeconds.setText("");
-    }
 
     /**
      * Startet den Service und gibt dem Intent als Extra die Laufzeit des Timers in Sekunden mit.
@@ -164,28 +121,12 @@ public class TimerActivity extends AppCompatActivity implements TimerBroadcastLi
      * @param timeInSeconds Laufzeit des Timers.
      */
     private void startTimerForTime(int timeInSeconds) {
-        btnStartTimer.setEnabled(false);
-        btnStopTimer.setEnabled(true);
-        txtTimer.setText(Timer.getFormattedStringFromInt(this, timeInSeconds));
-        Intent intent = new Intent(this, TimerService.class);
-        intent.putExtra(Timer.TIME_KEY, timeInSeconds);
-        startForegroundService(intent);
-    }
-
-    /**
-     * Überprüft die Angeben der Eingabefelder.
-     *
-     * @return True, wenn alle Angaben korrekt sind.
-     */
-    private boolean checkInputs() {
-        int[] setTime = getSetTime();
-        int h = setTime[0], m = setTime[1], s = setTime[2];
-        return h <= 99 && m <= 59 && s <= 59;
+        //TODO: Starten Sie hier ihren Timer.
     }
 
     /**
      * Liest die Eingaben aus den 3 Eingabefeldern aus und liefert ein int-Array zurück, welches die
-     * 3 Werte beinhaltet.
+     * 3 Werte beinhaltet. Sollte ds Feld leer sein wird als Standard-Wert 0 zurückgegeben
      *
      * @return int-Array mit den Werten für Stunden, Minuten und Sekunden.
      */
@@ -211,33 +152,19 @@ public class TimerActivity extends AppCompatActivity implements TimerBroadcastLi
     }
 
 
-    /**
-     * Resets the Buttons and the timer-TextView while also allowing to send a Toast to the user
-     *
-     * @param message The toasts message.
-     */
-    private void resetAndNotify(String message) {
-        txtTimer.setText("");
-        btnStopTimer.setEnabled(false);
-        btnStartTimer.setEnabled(true);
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
 
     @Override
     public void onTimerUpdate(int remainingTimeInSeconds) {
-        runOnUiThread(() -> txtTimer.setText(Timer.getFormattedStringFromInt(this, remainingTimeInSeconds)));
     }
 
     @Override
     public void onTimerFinished() {
         stateStorage.setTimerState(TimerState.IDLE);
-        runOnUiThread(() -> resetAndNotify(getString(R.string.toast_timer_finished)));
     }
 
 
     @Override
     public void onTimerCancelled() {
         stateStorage.setTimerState(TimerState.IDLE);
-        runOnUiThread(() -> resetAndNotify(getString(R.string.toast_timer_cancelled)));
     }
 }
