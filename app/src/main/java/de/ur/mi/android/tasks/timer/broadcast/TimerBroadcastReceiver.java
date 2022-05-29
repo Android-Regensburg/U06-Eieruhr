561d0990457c74500cd9f8391807240f7d6a496b
Alexander Bazo <alexanderbazo@googlemail.com>
2022-05-29 16:28:36 +0200

    Update comments for Receiver and Listener

--- app/src/main/java/de/ur/mi/android/tasks/timer/broadcast/TimerBroadcastReceiver.java
+++ app/src/main/java/de/ur/mi/android/tasks/timer/broadcast/TimerBroadcastReceiver.java
@@ -6,45 +6,59 @@ import android.content.Intent;
 import android.content.IntentFilter;
 
 /**
- * Der BroadcastReceiver empfängt Broadcasts anderer Android-Apps oder, in diesem Fall, eines Hintergrundservices unserer App.
- * Hier soll bei Änderung oder Ablauf des Timers später die TimerActivity informiert werden.
+ * Dieser Broadcast-Receiver empfängt Broadcasts anderer Stellen unserer Anwendung. Konkret gehen hier
+ * die Statusmeldungen des Timers ein, die aus dem Hintergrundservice verschickt werden. Der Receiver
+ * informiert angeschlossene Listener (TimerBroadcastListener) auf Basis dieser Broadcast, wenn der
+ * laufende Timer vollständig abgelaufen ist oder vorzeitig abgebrochen wurde. Zusätzlich werden die
+ * regelmäßigen Updates zur noch verbleibenden Zeit des Times weitergegeben.
  */
 public class TimerBroadcastReceiver extends BroadcastReceiver {
 
-    /**
-     * Konstanten zur Unterscheidung der Broadcast-Nachrichten
-     */
+    // Action-Name für Broadcast zur Kommunikation der verbleibenden Zeit des laufenden Timers
     private static final String TIMER_UPDATE = "de.ur.mi.android.task.timer.TIMER_UPDATE";
+    // Action-Name für Broadcast zur Kommunikation des erfolgreichen Durchlaufs des Timers
     private static final String TIMER_FINISHED = "de.ur.mi.android.task.timer.TIMER_FINISHED";
+    // Action-Name für Broadcast zur Kommunikation des vorzeitigen Abbruchs des Timers
     private static final String TIMER_CANCELLED = "de.ur.mi.android.task.timer.TIMER_CANCELLED";
+    // Schlüssel zum Speichern/Auslesen der verbleibenden Zeit im entsprechenden Broadcast
     private static final String REMAINING_TIME_IN_SECONDS = "REMAINING_TIME_IN_SECONDS";
 
-
-    /**
-     * Über den TimerBroadcastListener kann die Activity informiert werden
-     */
+    // Listener, der vom Receiver über die eingehenden Status-Updates informiert werden soll
     private final TimerBroadcastListener listener;
 
+    /**
+     * Erzeugt einen neuen BroadcastReceiver. Der übergebene Listener wird im Anschluss über die
+     * als Broadcasts eingehenden Status-Updates des Timers informiert.
+     *
+     * @param listener Listener, der vom Receiver über Status-Updates informiert werden soll.
+     */
     public TimerBroadcastReceiver(TimerBroadcastListener listener) {
         this.listener = listener;
     }
 
 
-
     /**
-     * Die onReceive-Callback-Methode wird aufgerufen, wenn Broadcasts mit den Aktionsbezeichnern empfangen werden,
-     * für die die Activity sich registriert. Je nach Action wird die jeweilige Callback-Methode des EggTimerBroadcastListeners gecalled
+     * Hier werden die eingehenden Broadcast, für die dieser Receiver ausgelegt ist
+     * (siehe getIntentFilter) abgefangen. Die Methode wird jedes mal aufgerufen, wenn ein passender
+     * Broadcast verschickt wurde. Über den übergebenen Intent kann die Art des Broadcasts und die
+     * ggf. enthaltenden Informationen ausgelesen werden.
+     *
+     * @param context Context, in dem der Receiver ausgeführt wird
+     * @param intent  Intent, der den empfangenen Broadcast repräsentiert
      */
     @Override
     public void onReceive(Context context, Intent intent) {
         switch (intent.getAction()) {
+            // Broadcast: Verbleibende Zeit des laufenden Timers hat sich geändert
             case TIMER_UPDATE:
                 int remainingTimeInSeconds = intent.getExtras().getInt(REMAINING_TIME_IN_SECONDS);
                 listener.onTimerUpdate(remainingTimeInSeconds);
                 break;
+            // Broadcast: Laufender Timer ist vollständig abgelaufen
             case TIMER_FINISHED:
                 listener.onTimerFinished();
                 break;
+            // Broadcast: Laufender Timer wurde vorzeitig abgebrochen
             case TIMER_CANCELLED:
                 listener.onTimerCancelled();
                 break;
@@ -52,7 +66,10 @@ public class TimerBroadcastReceiver extends BroadcastReceiver {
     }
 
     /**
-     * Durch IntentFilter kann bestimmt wird, für welche Art von Broadcasts die App bzw. die Activity sich interessiert
+     * Die Methode liefert einen IntentFilter zurück, über den beim Registrieren des Receivers in
+     * der App (Vgl. TimerActivity, Methode registerBroadcastReceiver) festgelegt werden kann, welche
+     * Broadcasts der Receiver empfangen soll. So stellen wir sicher, dass dieser Receiver später nur
+     * auf die von uns versendeten Broadcasts reagiert, die über den Zustand des Timers informieren.
      */
     public static IntentFilter getIntentFilter() {
         IntentFilter filter = new IntentFilter();
@@ -63,19 +80,23 @@ public class TimerBroadcastReceiver extends BroadcastReceiver {
     }
 
     /**
-     * Erzeugt einen Intent, der diesen BroadcastReceiver darüber informiert, wieviele Sekunden im
-     * Timer noch verbleiben.
+     * Erzeugt einen Intent, über den ein Broadcast mit der verbleibenden Zeit des laufenden Timers
+     * versendet werden kann. Die statische Methode erlaubt anderen Stellen der App eine entsprechende
+     * Nachricht zu versenden, die dann von den registrierten Receivern abgefangen und verarbeitet werden
+     * kann.
+     *
+     * @param remainingTimeInSeconds Die verbleibende Zeit des Timers in Sekunden.
      */
     public static Intent getUpdateIntent(int remainingTimeInSeconds) {
         Intent intent = new Intent();
         intent.setAction(TIMER_UPDATE);
-        intent.putExtra(REMAINING_TIME_IN_SECONDS,remainingTimeInSeconds);
+        intent.putExtra(REMAINING_TIME_IN_SECONDS, remainingTimeInSeconds);
         return intent;
     }
 
     /**
-     * Erzeugt einen Intent, der diesen BroadcastReceiver darüber informiert, dass die Zeit im Timer
-     * vollständig abgelaufen ist.
+     * Erzeugt einen Intent, über den ein Broadcast versendet werden kann, der über den erfolgreichen
+     * Abschluss des Timers informiert.
      */
     public static Intent getEndIntent() {
         Intent intent = new Intent();
@@ -84,8 +105,8 @@ public class TimerBroadcastReceiver extends BroadcastReceiver {
     }
 
     /**
-     * Erzeugt einen Intent, der diesen BroadcastReceiver darüber informiert, dass der Timer
-     * abgebrochen wurde.
+     * Erzeugt einen Intent, über den ein Broadcast versendet werden kann, der über den vorzeitigen
+     * Abbruch des Timers informiert.
      */
     public static Intent getCancelledIntent() {
         Intent intent = new Intent();
